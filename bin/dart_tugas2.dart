49a9b5e070e1c112d6f6148c6311c6887ac125e7
Skypros98 <muhammadagusdwifasophian_2226250085@mhs.mdp.ac.id>
2023-10-05 10:04:48 +0700

    Initial Project

---
+++ bin/dart_tugas2.dart
@@ -0,0 +1,18 @@
+// Nama : Muhammad Agus Dwifa
+// NPM  : 2226250085
+
+import 'PersegiPanjang.dart';
+
+void main() {
+  int panjang = 5;
+  int lebar = 3;
+
+  PersegiPanjang persegi = PersegiPanjang(panjang, lebar);
+
+  int luas = persegi.hitungLuas();
+  int keliling = persegi.hitungKeliling();
+
+  print('Panjang Persegi Panjang : $panjang Lebar Persegi Panjang : $lebar');
+  print('Luas Persegi Panjang : $luas');
+  print('Keliling Persegi Panjang : $keliling');
+}
